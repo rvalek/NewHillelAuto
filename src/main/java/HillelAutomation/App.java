ed6c9e0a33f01bba76da2c766977df68ec0fd0da
Robert Valek <rvalek@intersog.com>
2018-02-15 21:28:38 +0200

    First commit

---
+++ src/main/java/HillelAutomation/App.java
@@ -0,0 +1,13 @@
+package HillelAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
