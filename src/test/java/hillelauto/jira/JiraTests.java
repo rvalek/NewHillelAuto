8d48c0610ad35aa33d5cbd492760ae7297c9bd03
Robert Valek <rvalek@intersog.com>
2018-03-15 19:50:07 +0200

    Moved testrail before/after to WebDriverTestBase

--- src/test/java/hillelauto/jira/JiraTests.java
+++ src/test/java/hillelauto/jira/JiraTests.java
@@ -1,51 +1,20 @@
 package hillelauto.jira;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.openqa.selenium.support.PageFactory;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import hillelauto.WebDriverTestBase;
-import hillelauto.reporting.TestRail;
 
 public class JiraTests extends WebDriverTestBase {
     private LoginPage loginPage;
     private IssuePage issuePage;
-
-    private TestRail trReport;
 
     @BeforeClass(alwaysRun = true)
     public void initPages() {
         loginPage = PageFactory.initElements(browser, LoginPage.class);
         issuePage = PageFactory.initElements(browser, IssuePage.class);
         System.out.println("Jira Pages Initialized");
-    }
-
-    @BeforeClass(groups = "TestrailReport")
-    protected void prepareTestRailRun() throws Exception {
-        String baseURL = "https://hillelrob.testrail.io/";
-        System.out.println("Reporting to " + baseURL);
-
-        trReport = new TestRail(baseURL);
-        trReport.setCreds("rvalek@intersog.com", "hillel");
-        trReport.startRun(1, "Jira Auto - " + new SimpleDateFormat("dd/MM/yy HH:mm").format(new Date()));
-    }
-
-    @AfterMethod(groups = "TestrailReport")
-    protected void reportResult(ITestResult testResult) throws Exception {
-        String testDescription = testResult.getMethod().getDescription();
-        trReport.setResult(Integer.parseInt(testDescription.substring(0, testDescription.indexOf("."))),
-                testResult.getStatus());
-    }
-
-    @AfterClass(groups = "TestrailReport")
-    protected void closeTestRailRun() throws Exception {
-        trReport.endRun();
     }
 
     @Test(description = "1. Invalid Login", priority = -1)
