9442a72de485d254ca3074f3506c1db1b309cbcd
Robert Valek <rvalek@intersog.com>
2018-03-18 14:06:02 +0200

    Moved testrail reporting to listeners

--- src/test/java/hillelauto/WebDriverTestBase.java
+++ src/test/java/hillelauto/WebDriverTestBase.java
@@ -10,7 +10,6 @@ import org.testng.annotations.BeforeTest;
 
 public class WebDriverTestBase {
     protected WebDriver browser;
-    // private TestRail trReport;
 
     static {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
@@ -28,32 +27,4 @@ public class WebDriverTestBase {
     public void finish() {
         browser.close();
     }
-
-    // @Parameters({ "testRailProjectId", "testRailRunPrefix" })
-    // @BeforeTest(groups = "TestRailReport")
-    // public void prepareTestRailRun(String projectId, String runPrefix) throws Exception {
-    //     String baseURL = "https://hillelrob.testrail.io/";
-    //     System.out.println("Reporting to " + baseURL);
-
-    // trReport = new TestRail(baseURL);
-    // trReport.setCreds("rvalek@intersog.com", "hillel");
-    // trReport.startRun(Integer.parseInt(projectId), runPrefix + " Robert Auto - " + Tools.timestamp());
-    // }
-
-    // @AfterMethod(groups = "TestRailReport")
-    // protected void reportResult(ITestResult testResult) throws Exception {
-    //     String testDescription = testResult.getMethod().getDescription();
-    //     try {
-    //         int caseId = Integer.parseInt(testDescription.substring(0, testDescription.indexOf(".")));
-    //         trReport.setResult(caseId, testResult.getStatus());
-    //     } catch (IndexOutOfBoundsException | NumberFormatException e) {
-    //         System.out.println(testDescription + " - Case ID missing; not reporting to TestRail.");
-    //     }
-
-    // }
-
-    // @AfterTest //(groups = "TestRailReport")
-    // public void closeTestRailRun() throws Exception {
-    //     // trReport.endRun();
-    // }
 }
