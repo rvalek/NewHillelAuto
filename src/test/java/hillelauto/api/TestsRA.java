6dd7cd1d076b1cc93887c3fea2a791ae973c88c4
Robert Valek <rvalek@intersog.com>
2018-12-14 23:38:51 +0200

    A little more rest assured stuff

--- src/test/java/hillelauto/api/TestsRA.java
+++ src/test/java/hillelauto/api/TestsRA.java
@@ -8,7 +8,6 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 
 public class TestsRA {
     @BeforeClass
@@ -18,20 +17,31 @@ public class TestsRA {
 
     @DataProvider
     public Object[][] newUserData() {
-        return new Object[][] { { "{\"name\":\"Test\",\"phone\":\"1111\",\"role\":\"Student\"}" } };
+        return new Object[][] { { "{\"name\":\"Test Student\",\"phone\":\"1111\"}" },
+                { "{\"name\":\"Test Admin\",\"phone\":\"1111\",\"role\":\"Administrator\"}" },
+                { "{\"name\":\"Test Support\",\"phone\":\"1111\",\"role\":\"Support\"}" } };
     }
 
     @Test(description = "Fourth requirement - creating users", dataProvider = "newUserData")
-    void createUser(String userData) {
-        RequestSpecification req = RestAssured.given().header("Content-Type", "application/json");
-        req.body(userData);
+    void createUser(String userData, String userRole) {
+        Response res = RestAssured.given().header("Content-Type", "application/json").body(userData).post("POST", "/");
 
-        Response res = req.request("POST", "/");
+        res.peek().then().statusCode(200).header("Content-Type", "application/json; charset=utf-8").body("id", Is.isA(Integer.class));
+
+        RestAssured.given().header("Content-Type", "application/json").request("GET", "/").then()
+                .body(CoreMatchers.containsString(res.getBody().asString()));
+
+        // RequestSpecification req = RestAssured.given();
+        // req.header("Content-Type", "application/json");
+        // req.body(userData);
+
+        // Response res = req.request("POST", "/");
         // res.peek();
-        res.then().header("Content-Type", "application/json; charset=utf-8").body("id", Is.isA(Integer.class));
+        // res.then().header("Content-Type", "application/json;
+        // charset=utf-8").body("id", Is.isA(Integer.class));
 
-        String newUser = res.getBody().asString();
-        req.request("GET", "/").then().body(CoreMatchers.containsString(newUser));
+        // String newUser = res.getBody().asString();
+        // req.request("GET", "/").then().body(CoreMatchers.containsString(newUser));
     }
 
 }
